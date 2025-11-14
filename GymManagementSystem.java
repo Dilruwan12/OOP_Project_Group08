ac2550fc64f2064f67202053ec2a4657ba6cc91a
janindusanjana <janindusanjanagallage@gmail.com>
2025-11-14 13:40:54 +0530

    txt file path added

--- GymManagementSystem.java
+++ GymManagementSystem.java
@@ -1,8 +1,13 @@
 import java.util.*;
 import java.io.*;
+import java.lang.reflect.Member;
 
 public class GymManagementSystem {
     private ArrayList<Member> members = new ArrayList<>();
     private ArrayList<Trainer> trainers = new ArrayList<>();
     private ArrayList<Payment> payments = new ArrayList<>();
+
+    private final String MEMBER_FILE = "C:\\Java\\GymmanagementSystem\\data\\member.txt";
+    private final String PAYMENT_FILE = "C:\\Java\\GymmanagementSystem\\data\\payments.txt";
+
 }
